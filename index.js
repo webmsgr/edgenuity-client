4dd88dda3a3496d4dee3e7799186f30b526bd5f2
Restyled.io <commits@restyled.io>
2020-12-30 21:38:08 +0000

    Restyled by prettier

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-const {app, BrowserWindow} = require('electron')
-const fs = require('fs')
-const {Base64} = require('js-base64');
+const { app, BrowserWindow } = require("electron");
+const fs = require("fs");
+const { Base64 } = require("js-base64");
 // skipper = Base64.encode(fs.readFileSync("js/skipper/release/skipper.min.js"))
 
 // waitforinject = fs.readFileSync("js/waitforinject.js","ascii")
 // waitforinject = waitforinject.replace("$code",skipper)
 function createWindow() {
-  const win = new BrowserWindow(
-      {width : 800, height : 600, webPreferences : {nodeIntegration : false}})
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: { nodeIntegration: false },
+  });
 
-  win.loadURL('https://www.edgenuity.com/')
+  win.loadURL("https://www.edgenuity.com/");
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
-app.on("web-contents-created",
-       (event, contents) => {contents.on(
-           "dom-ready",
-           () => {
-               url = contents.getURL()
-               // if (url == "https://r05.core.learn.edgenuity.com/Player/") {
-               //  contents.executeJavaScript(waitforinject).then((e)
-               //  => {console.log("Injected")})
-               // }
-           })})
+app.on("web-contents-created", (event, contents) => {
+  contents.on("dom-ready", () => {
+    url = contents.getURL();
+    // if (url == "https://r05.core.learn.edgenuity.com/Player/") {
+    //  contents.executeJavaScript(waitforinject).then((e)
+    //  => {console.log("Injected")})
+    // }
+  });
+});
--- modules/skipper/waitforinject.js
+++ modules/skipper/waitforinject.js
@@ -1,14 +1,16 @@
-
-
 function inject() {
-  if (typeof document.querySelector("#stageFrame").contentWindow.API.Video !==
-      undefined) {
+  if (
+    typeof document.querySelector("#stageFrame").contentWindow.API.Video !==
+    undefined
+  ) {
     $.getScript(
-        "https://webmsgr.github.io/edgenuity-skipper/release/skipper.min.js")
-    clearInterval(window.injectitv)
+      "https://webmsgr.github.io/edgenuity-skipper/release/skipper.min.js"
+    );
+    clearInterval(window.injectitv);
   }
 }
 
-window.injectitv = setInterval(inject, 100)
-document.querySelector("#stageFrame")
-    .addEventListener("load", (evt) => {setTimeout(inject, 1000)})
+window.injectitv = setInterval(inject, 100);
+document.querySelector("#stageFrame").addEventListener("load", (evt) => {
+  setTimeout(inject, 1000);
+});
